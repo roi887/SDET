70cf745ba9231934e5b10fdd39a949a057d72219
Arun <myEmail@example.com>
2025-04-27 23:55:36 +0530

    again changes

--- Jeevan.java
+++ Jeevan.java
@@ -5,6 +5,10 @@ public class Jeevan {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int as =89;
+		int b =67;
+		int c=b+as;
+		System.out.println(c);
 	}
 
 }
