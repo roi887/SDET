2c1955c74d314773095247ee2b074163ac65a6a3
Arun <myEmail@example.com>
2025-05-11 22:29:49 +0530

    one more file

---
+++ vari.java
@@ -0,0 +1,14 @@
+package com.Java;
+
+public class vari {
+
+    public static void main(String [] args){
+
+        int a =9;
+        String c = "where are you ";
+
+        System.out.println(a);
+        System.out.println(c);
+
+    }
+}
