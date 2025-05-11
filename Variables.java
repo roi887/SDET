f05cd86b09a4d79279c5f6e0364ca191d73d732e
Arun <myEmail@example.com>
2025-05-11 21:24:38 +0530

    variable file

---
+++ Variables.java
@@ -0,0 +1,16 @@
+package com.Java;
+
+import java.util.Scanner;
+
+public class Variables {
+    public static void main(String[] args) {
+    int a,i,output;
+    Scanner scanner=new Scanner(System.in);
+        System.out.printf("Enter the table no:");
+    a=scanner.nextInt();
+        for(i=1;i<=10;i++)
+    {
+        output=a*i;
+        System.out.printf("%dx%d=%d%n",a,i,output);
+    }
+    }}
