3d28fa2527a0ee168d247af217525d6be57edf00
Arun <myEmail@example.com>
2025-06-01 20:27:13 +0530

    new file

---
+++ reverse.java
@@ -0,0 +1,20 @@
+package com.project12;
+
+public class reverse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 String s1 ="arun";;
+
+	        String reversed = "";
+	        for (int i = s1.length()-1; i >= 0; i--) {
+	            reversed = reversed+(s1.charAt(i));  // appending character at the end
+	        }
+
+	        System.out.println("Reversed: " + reversed);
+
+
+	}
+
+}
